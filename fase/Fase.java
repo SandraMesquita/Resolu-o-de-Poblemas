3dd498ec702328300cf6af2c0627f4b3d07e83ba
Sandra Mesquita <42285647+SandraMesquita@users.noreply.github.com>
2019-05-30 23:52:26 -0400

    Refatoracao do código inicial

--- fase/Fase.java
+++ fase/Fase.java
@@ -1,28 +1,46 @@
-package fase;
 /**
  * @author Sandra Mesquita
  */
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-public class Fase {
+public class bla {
 
     public static void main(String[] args) {
        Scanner teclado = new Scanner (System.in);
-        Lista list = new Lista ();
+     //qtdNum = quantidade de competidoes.
+     //qtdCamo = quantidade de Campeoes (clssificados).
        int qtdNum, qtdCamp ;
        
        qtdNum = teclado.nextInt();
        qtdCamp = teclado.nextInt();
        
+       ArrayList<String> competidores = new ArrayList<String>(); 
        for (int i = 0; i < qtdNum; i++){
            int numero = teclado.nextInt();
-           list.inserir(numero);
+           competidores.add("" + numero);
        }
-              
-       System.out.println (list.imprimir(qtdCamp) );
-       System.out.print (list.imprimir() );
+       // ordenando de forma decescente.
+       Collections.sort(competidores, Collections.reverseOrder());       
        
        
-       
+       int count = 0;
+       String str = "";
+        
+        //verificando os classificados.
+       while( count < competidores.size()){
+           
+           
+           if (count < qtdCamp || competidores.get(count).equalsIgnoreCase(competidores.get(count -1) ) ){
+                count ++;
+                str = ""+ count;
+            }
+           else
+               break;
+        
+       }
+       System.out.println (str );
+             
     }
     
 }
